4b16ebc4a81ec2c465cc4ef250dc9cad201aa0c8
Samuel Maginot <samagino@indiana.edu>
2019-07-23 16:08:32 -0400

    Updates

---
+++ precache-manifest.092c064cab441d9e9e3d3ebf3c4747bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87ebbb00b1fb467ca5e468a1f8a4fbf",
+    "url": "/tableMethodThing/index.html"
+  },
+  {
+    "revision": "2d524b8c0562c770d329",
+    "url": "/tableMethodThing/static/css/main.571eb442.chunk.css"
+  },
+  {
+    "revision": "852fe4682fe2daf24902",
+    "url": "/tableMethodThing/static/js/2.67dcd455.chunk.js"
+  },
+  {
+    "revision": "2d524b8c0562c770d329",
+    "url": "/tableMethodThing/static/js/main.404f940e.chunk.js"
+  },
+  {
+    "revision": "42d4c812705356f6297f",
+    "url": "/tableMethodThing/static/js/runtime~main.7632f906.js"
+  }
+]);
