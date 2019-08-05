0736a0651fa118c9e043a2b048210931c4c85bf2
Samuel Maginot <samagino@indiana.edu>
2019-08-05 16:36:33 -0400

    Updates

--- precache-manifest.de1c6126e7b42ebb914b70f7f8297f22.js
+++ precache-manifest.de1c6126e7b42ebb914b70f7f8297f22.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ef9682115f4a59fbaf7ffdbac1bac92c",
+    "revision": "4e121989274cedac6770e8a66605e3d2",
     "url": "/tableMethodThing/index.html"
   },
   {
-    "revision": "7d7cb06764040b81b81b",
+    "revision": "6021ab68662b436ca7df",
     "url": "/tableMethodThing/static/css/main.571eb442.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tableMethodThing/static/js/2.1a7136d6.chunk.js"
   },
   {
-    "revision": "7d7cb06764040b81b81b",
-    "url": "/tableMethodThing/static/js/main.e1a23db6.chunk.js"
+    "revision": "6021ab68662b436ca7df",
+    "url": "/tableMethodThing/static/js/main.16fbbd0c.chunk.js"
   },
   {
     "revision": "42d4c812705356f6297f",
