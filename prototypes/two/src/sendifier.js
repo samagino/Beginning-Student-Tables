e532185616fa2bc5d62ad910370d016cdad207ef
Chung-chieh Shan <ccshan@indiana.edu>
2019-09-30 20:46:57 -0400

    Improve session logging
    Retry on failure (using the latest data)
    
    Wait 3 seconds between the END of one successful or failed attempt and
    the beginning of the next attempt

--- prototypes/two/src/sendifier.js
+++ prototypes/two/src/sendifier.js
@@ -1,24 +1,33 @@
-// Number -> (String -> Side Effect)
+// Number String -> (String -> Side Effect)
 function makeSendifier (delay, sessionID) {
     const url = "http://107.170.76.216:8000/log/session" + sessionID;
     let item = false;
 
-    function setItem (nuveau) {
-        item = nuveau;
+    function setItem (news) {
+        item = news;
     }
 
     function sendItem () {
         if (item) {
+            const sent = item;
             fetch(url, { method: 'POST', // or 'PUT'
-                         body: item, // data can be `string` or {object}!
+                         body: sent, // data can be `string` or {object}!
                          mode: 'no-cors',
-                         headers: { 'Content-Type': 'application/json' } });
+                         headers: { 'Content-Type': 'application/json' } })
+            .then(function () {
+                      setTimeout(sendItem, delay);
+                  },
+                  function () {
+                      if (!item) item = sent;
+                      setTimeout(sendItem, delay);
+                  });
+            item = false;
+        } else {
+            setTimeout(sendItem, delay);
         }
-
-        item = false;
     }
 
-    setInterval(sendItem, delay);
+    sendItem();
 
     return setItem;
 }
