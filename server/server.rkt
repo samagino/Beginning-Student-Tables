968bf43c0b2e7b1cf84cba9a9018851710006fea
Sam Tobin-Hochstadt <samth@indiana.edu>
2019-10-07 00:31:24 -0400

    Add server code.

---
+++ server/server.rkt
@@ -0,0 +1,57 @@
+#lang racket/base
+
+(require racket/runtime-path web-server/servlet-env
+         response-ext web-server/dispatch
+	 racket/port
+         web-server/http/response-structs
+         web-server/http/request-structs)
+
+(define-runtime-path log-dir "logs")
+
+(define (response/404)
+  (response/output #:code 404 (lambda (o) (write "Bad path." o))))
+
+
+(define (response/file file #:headers [headers '()] [mime TEXT/HTML-MIME-TYPE])
+  (response/output (λ (op) (let ([ip (open-input-file file)]) (copy-port ip op) (close-input-port ip))) 
+		   #:mime-type mime
+		   #:headers headers))
+
+
+(define-values (dispatch u)
+  (dispatch-rules
+   [("list") #:method "get" list-logs]
+   [("log" (string-arg)) #:method "post" save]
+   [("log" (string-arg)) #:method "get" get]))
+
+(define (save req id)
+  (cond [(regexp-match "^[a-zA-Z0-9]+$" id)
+         (define data (request-post-data/raw req))
+         (when data
+           (call-with-output-file (build-path log-dir id)
+	     #:exists 'truncate
+             (lambda (o)
+               (write-bytes data o))))
+         (response/make "it worked!")]
+        [(response/404)]))
+
+(define (list-logs req)
+  (define l (directory-list log-dir))
+  (response/json (map path->string l)))
+
+(define (get req id)
+  (log-error "id is: ~s" id)
+  (cond [(regexp-match "^[a-zA-Z0-9]+$" id)
+         (define f (build-path log-dir id))
+	 (if (file-exists? f)
+	     (response/file f #"text/plain" 
+			    #:headers (list (make-header #"Access-Control-Allow-Origin" #"*")))
+	     (response/404))]
+        [else
+         (response/404)]))
+  
+
+(serve/servlet dispatch
+	       #:servlet-path "/"
+	       #:servlet-regexp #rx""
+	       #:listen-ip #f)
